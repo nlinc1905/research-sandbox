8efb5ffedd9a0222e0d3992eedf6333229044531
nick <nlinc1905@gmail.com>
2025-05-07 14:00:54 -0400

    second celery task

--- api_with_long_running_task/src/main.py
+++ api_with_long_running_task/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.tasks import long_running_task
+from src.tasks import long_running_task, addi
 
 app = FastAPI()
 
@@ -13,6 +13,22 @@ def start_task():
 @app.get("/task-status/{task_id}")
 def get_task_status(task_id: str):
     result = long_running_task.AsyncResult(task_id)
+    return {
+        "task_id": task_id,
+        "status": result.status,
+        "result": result.result if result.ready() else None
+    }
+
+
+@app.post("/add")
+def add_numbers(x: int, y: int):
+    task = addi.delay(x, y)
+    return {"task_id": task.id}
+
+
+@app.get("/add-status/{task_id}")
+def get_add_status(task_id: str):
+    result = addi.AsyncResult(task_id)
     return {
         "task_id": task_id,
         "status": result.status,
--- api_with_long_running_task/src/tasks.py
+++ api_with_long_running_task/src/tasks.py
@@ -5,7 +5,8 @@ from celery import Celery
 celery_app = Celery(
     "worker",
     broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0"
+    backend="redis://redis:6379/0",
+    include=["src.tasks"],
 )
 
 
@@ -13,3 +14,8 @@ celery_app = Celery(
 def long_running_task():
     time.sleep(30)
     return "Task completed after 30 seconds"
+
+
+@celery_app.task
+def addi(x, y):
+    return x + y
